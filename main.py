93719e17f54866111867ec6abe865aa844c6dde7
Maksim Trostin <92330982+Argentusz@users.noreply.github.com>
2022-04-17 03:31:51 +0300

    Аппрув наброска. Нахождение зависимости F от p_mut
    TODO: зависимости от всего и графики =)

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def W_calc(k, N, M):
         W.append([])
         for j in range(M):
             p = np.random.randint(0, 101)
-            if p < 100*k/N:
-                if p < (k/(2*N))*100:
+            if p < 100 * k / N:
+                if p < (k / (2 * N)) * 100:
                     W[i].append(1)
                 else:
                     W[i].append(-1)
@@ -18,7 +18,7 @@ def W_calc(k, N, M):
 
 
 def sigma_calc(z):
-    return 1/(1 + np.exp(z))
+    return 1 / (1 + np.exp(z))
 
 
 def f_calc(M, N, W, S, h):
@@ -35,12 +35,12 @@ def f_calc(M, N, W, S, h):
 def w_tr_calc(C, f, B, M):
     lineal = 0
     for i in range(M):
-        lineal += C[i]*f[i]
+        lineal += C[i] * f[i]
     double_sum = 0
     for i in range(M):
         for j in range(M):
             double_sum += B[i][j] * f[i] * f[j]
-    return lineal + double_sum/2
+    return lineal + double_sum / 2
 
 
 def mutate(S, p_mut):
@@ -59,48 +59,45 @@ def d_alg(M, N, k, h, p_mut):
     S = []
     for i in range(N):
         S.append(np.random.randint(0, 2))
-    print("S = ", S)
     # Высчитываем рандомный вектор C
     C = []
     for i in range(M):
         C.append(np.random.randint(0, 2))
-    print("C = ", C)
     # Высчитываем рандомную симметричную матрицу B
     B_asymmetric = np.random.randint(0, 2, size=(M, M), dtype='int')
     B = (B_asymmetric + B_asymmetric.T) % 2
-    print("B:")
-    print(B)
     # Высчитываем рандомную матрицу W
     W = np.matrix(W_calc(k, N, M))
-    print("W:")
-    print(W)
     # Высчитываем изначальный вектор фенотипа f
     f = f_calc(M, N, W, S, h)
-    print("f = ", f)
     # S_new будет мутированный генотип с которым будем сравнивать старый
     S_new = []
     for i in range(N):
         S_new.append(S[i])
 
-    # Здесь будет куча раз
-    for t in range(1):
+    for t in range(250):
         # Мутируем гены
         mutate(S_new, p_mut)
-        print("S_new = ", S_new)
         # Высчитываем новый фенотип
         f_new = f_calc(M, N, W, S_new, h)
-        print("f_new = ", f_new)
         # Сравниваем нужно ли мутировать
         if w_tr_calc(C, f, B, M) > w_tr_calc(C, f_new, B, M):
-            print("Mutated.")
             for i in range(N):
                 S[i] = S_new[i]
         else:
-            print("Not Mutated.")
             for i in range(N):
                 S_new[i] = S[i]
-    print("S = ", S)
+    return w_tr_calc(C, f, B, M)
 
 
 if __name__ == '__main__':
-    d_alg(5, 10, 5, 2, 50)
+    # Looking for dependence of F from p_mut
+
+    # Consts in this case
+    M_main, N_main, k_main, h_main = 5, 10, 5, 2
+    p_mut_main = 10
+    print(f'{M_main=}, {N_main=}, {k_main=}, {h_main=}')
+    while p_mut_main <= 100:
+        print(f'{p_mut_main=} Got:',
+              d_alg(M_main, N_main, k_main, h_main, p_mut_main))
+        p_mut_main += 10
